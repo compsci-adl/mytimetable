d5644fc1aea3415ce8123f1c456b917d5e7adff1
jsun969 <i@jsun.lol>
2024-07-03 16:55:36 +0800

    chore: format & lint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,32 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
-}
+	root: true,
+	env: { browser: true, es2020: true },
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:react-hooks/recommended',
+		'plugin:prettier/recommended',
+	],
+	ignorePatterns: ['dist', '.eslintrc.cjs'],
+	parser: '@typescript-eslint/parser',
+	plugins: ['react-refresh'],
+	ignorePatterns: ['*.cjs', '*.mjs'],
+	rules: {
+		'react-refresh/only-export-components': [
+			'warn',
+			{ allowConstantExport: true },
+		],
+		'@typescript-eslint/explicit-function-return-type': 'off',
+		'@typescript-eslint/explicit-module-boundary-types': 'off',
+		'@typescript-eslint/no-non-null-assertion': 'off',
+		'@typescript-eslint/no-unused-vars': ['warn', { ignoreRestSiblings: true }],
+		'@typescript-eslint/consistent-type-imports': [
+			'warn',
+			{ disallowTypeAnnotations: false },
+		],
+		'no-console': 'warn',
+		eqeqeq: 'warn',
+		'prefer-const': 'warn',
+		'no-var': 'warn',
+	},
+};
---
+++ .prettierrc.cjs
@@ -0,0 +1,12 @@
+/** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions & import('@trivago/prettier-plugin-sort-imports').PluginConfig} */
+module.exports = {
+	singleQuote: true,
+	trailingComma: 'all',
+	useTabs: true,
+	plugins: [
+		'@trivago/prettier-plugin-sort-imports',
+		'prettier-plugin-tailwindcss',
+	],
+	importOrder: ['<THIRD_PARTY_MODULES>', '^[./]'],
+	importOrderSeparation: true,
+};
