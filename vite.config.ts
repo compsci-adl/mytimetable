66e7bfd8dd6718822f9cb1088507d5e1c40c10cf
jsun969 <i@jsun.lol>
2024-09-18 19:33:36 +0930

    chore: vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	test: {
+		globals: true,
+	},
 });
