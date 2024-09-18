c3a991614ce3893ce1d11da9277743da6b2c40aa
jsun969 <i@jsun.lol>
2024-09-18 21:34:16 +0930

    chore(css): custom breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,11 @@ export default {
 		'./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
 	],
 	theme: {
-		extend: {},
+		extend: {
+			screens: {
+				mobile: { max: '767px' },
+			},
+		},
 	},
 	darkMode: 'class',
 	plugins: [nextui()],
