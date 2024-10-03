a9d73b79f3959564888a852a42435ffe1a9ceb2c
jsun969 <i@jsun.lol>
2024-10-04 01:28:11 +0930

    fix: zoom buttons position on mobile

--- src/components/ZoomButtons.tsx
+++ src/components/ZoomButtons.tsx
@@ -13,7 +13,7 @@ export const ZoomButtons = () => {
 	const { height, setHeight } = useCalendarHourHeight();
 
 	return (
-		<div className="fixed bottom-8 right-8 z-[100] flex flex-col gap-2">
+		<div className="fixed bottom-8 right-8 z-[100] flex flex-col gap-2 mobile:bottom-3 mobile:right-3">
 			<Tooltip content={t('zoom.zoom-in')} placement="left">
 				<Button
 					onClick={() => setHeight((h) => h + 0.5)}
